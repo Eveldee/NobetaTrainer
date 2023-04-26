41182fb8afb67d1eff42b416b97cc91bcb3e24e7
Eveldee <eveldee0680@live.fr>
2023-04-26 11:26:59 +0200

    Initial commit

---
+++ NobetaTrainer/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NobetaTrainer;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("LittleWitchNobeta")]
+public class Plugin : BasePlugin
+{
+    internal new static ManualLogSource Log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Harmony.CreateAndPatchAll(typeof(Plugin));
+    }
+
+    [HarmonyPatch(typeof(UIPauseMenu), "Appear")]
+    [HarmonyPrefix]
+    static void PauseMenuOpenPrefix()
+    {
+        Log.LogInfo("Pause menu opened");
+    }
+}
